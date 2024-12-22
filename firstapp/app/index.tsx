557012bc86da8a74203a4f16c180e1639faf54a3
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-12-22 22:38:13 +0530

    import image to the index.tsx

--- firstapp/app/index.tsx
+++ firstapp/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require('../assets/images/react-logo.png')}></Image>
+      <Text>Hello World!</Text>
+      <Text>My First React Native cross platform App</Text>
     </View>
   );
 }
